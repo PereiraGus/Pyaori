87004f3c8551c648a67e395e93d72ec4df88ad58
PereiraGus <gustavo.oito@hotmail.com>
2023-05-15 21:14:50 -0300

    Mais adições nas configurações

--- js/config.js
+++ js/config.js
@@ -5,7 +5,9 @@ const SLCT_CONFIG_PRONOMES = document.getElementById("pronomesConfig");
 
 function carregarInfosPerfil(){
     INP_CONFIG_NOME.value = perfil.nome;
+    INP_CONFIG_NOME.disable = true;
     INP_CONFIG_EMAIL.value = perfil.email;
+    INP_CONFIG_EMAIL.disable = true;
     ICON_CONFIG_MAIORIDADE.className = "fa-solid";
     if(perfil.maioridade){
         ICON_CONFIG_MAIORIDADE.className += " fa-check";
@@ -17,7 +19,14 @@ function carregarInfosPerfil(){
     var pronomes = ["Ela/Dela","Ele/Dele","Elu/Delu"];
     for(var i = 0; i < pronomes.length; i++){
         if(pronomes[i] == perfil.pronomes){
-            
+            SLCT_CONFIG_PRONOMES.innerHTML += `
+                <option value=${pronomes[i]} selected="selected">${pronomes[i]}</option>
+            `
+        }
+        else{
+            SLCT_CONFIG_PRONOMES.innerHTML += `
+                <option value=${pronomes[i]}>${pronomes[i]}</option>
+            `
         }
     }
 }
